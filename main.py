925fab55b897871efd7252a1439be0aad22739da
unknown <adamhe2004@gmail.com>
2024-03-18 16:36:00 +0200

    server and functions fixed

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from docx import Document
 from io import BytesIO
-from docx.oxml.ns import qn
-from PIL import Image
-
-
 from docx.shared import Inches
 from docx.oxml import parse_xml
 from docx.oxml.ns import nsdecls
-from docx.oxml import OxmlElement
-# function to replace the picture
-
+from docx.oxml.ns import qn
+from PIL import Image
+import base64
+from lxml import etree
 
 def add_img_to_cc(docx_content, signatures: dict):
     """
@@ -20,19 +17,22 @@ def add_img_to_cc(docx_content, signatures: dict):
         signatures (dict): Dictionary mapping content control names to signature images.
 
     Returns:
-        Document: Modified Word document.
+        bytes: Modified Word document content.
     """
     doc = Document(BytesIO(docx_content))
 
-    # Iterate over each content control and its corresponding image
     for cc_name, signature in signatures.items():
-        # Open the signature image using PIL
+        # Extract base64 data from the signature
+        base64_data = signature.split(",")[1]
+        image_data = base64.b64decode(base64_data)
         
-        image = Image.open(BytesIO(bytes(signature)))
+        # Open the image using PIL
+        image = Image.open(BytesIO(image_data))
+        
         # Add the image to the document
         width, height = image.size
-        doc.add_picture(BytesIO(bytes(signature)), width=Inches(width / 96))
-
+        doc.add_picture(BytesIO(image_data), width=Inches(width / 96))
+        
         # Remove the paragraph containing the image
         p = doc.paragraphs[-1]._element
         p.getparent().remove(p)
@@ -42,18 +42,19 @@ def add_img_to_cc(docx_content, signatures: dict):
         rid = keys[-1]
 
         # Find the content control with the specified name or skip if not found
-        for sdt in doc.element.xpath("//w:sdt"):
-            alias = sdt.xpath(".//w:alias[@w:val='%s']" % cc_name, namespaces=nsdecls)
+        for sdt in doc.element.xpath("w:sdt"):
+            alias = sdt.xpath("w:alias[@w:val='%s']" % cc_name)
             if alias:
                 # Set the relationship ID for the image
-                sdt.xpath(".//a:blipFill/a:blip", namespaces=nsdecls)[0].set(qn("r:embed"), rid)
+                blip = sdt.xpath("a:blipFill/a:blip")[0]
+                blip.set(qn("r:embed"), rid)
                 break
 
+    # Save the modified document content to a buffer
     modified_docx_buffer = BytesIO()
     doc.save(modified_docx_buffer)
     modified_docx_content = modified_docx_buffer.getvalue()                 
     return modified_docx_content
-
 
 
 def process_docx(fields:dict, docx_content:bytes) -> bytes:
