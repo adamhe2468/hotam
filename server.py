f2d2848e9c8ef898198ca2f4fd1253c5ff038932
unknown <adamhe2004@gmail.com>
2024-03-18 13:56:12 +0200

    added signatures

--- server.py
+++ server.py
@@ -15,7 +15,9 @@ def process():
     docx_content = base64.b64decode(request_content["file"]["$content"])
     docx_type =request_content["file"]["$content-type"]
     fields = request_content["fields"]
-  
+    law_sig = request_content["lawyer"]
+    costumer_sig = request_content["costumer"]
+
 
 # Parse the JSON string into a list of dictionaries
     json_list = json.loads(fields)
@@ -24,13 +26,17 @@ def process():
     dictionary = {}
     for item in json_list:
      dictionary[item['key']] = item['value']
+     # first fill the text fields
+    signatures= {"Picture1":law_sig,
+                 "Picture2": costumer_sig}
     modified_docx_content = process_docx(dictionary, docx_content)
-        
+    # insert the signatures
+    modified_docx_content =  add_img_to_cc(modified_docx_content,signatures)  
         # Encode the modified DOCX content
     modified_docx_encoded = base64.b64encode(modified_docx_content).decode('utf-8')
         
     return jsonify({"file": {    "$content-type": docx_type,"$content": modified_docx_encoded}})
-
+    
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=PORT)
     # serve(app, host = "0.0.0.0", port = PORT)
