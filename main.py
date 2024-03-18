e37ea301b00122ebb9c244167e104e167c0c74f8
unknown <adamhe2004@gmail.com>
2024-03-18 13:49:42 +0200

    signatures added

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
 from docx import Document
 from io import BytesIO
 from docx.oxml.ns import qn
-
+from PIL import Image
+from docx.shared import Inches
+from docx.oxml import parse_xml
+from docx.oxml.ns import nsdecls
+from docx.oxml import OxmlElement
 # function to replace the picture
 
 
-def add_img_to_cc(file_name, img_path):
+def add_img_to_cc(docx_content, signatures: dict):
+    """
+    Add images to content controls in a Word document.
 
-    doc = Document(file_name)
+    Args:
+        docx_content (bytes): Content of the Word document.
+        signatures (dict): Dictionary mapping content control names to signature images.
 
-    # adding the image and removing it to create a rel between the document and the image
-    doc.add_picture(img_path)
-    p = doc.paragraphs[-1]._element
-    p.getparent().remove(p)
+    Returns:
+        Document: Modified Word document.
+    """
+    doc = Document(BytesIO(docx_content))
 
-    # finding the id of the image
-    keys = list(doc.part.rels.keys())
+    # Iterate over each content control and its corresponding image
+    for cc_name, signature in signatures.items():
+        # Open the signature image using PIL
+        image = Image.open(BytesIO(bytes(signature)))
 
-    # the rid of the picture we wanna add its also the last key in the list
-    rid = keys[-1]
+        # Add the image to the document
+        width, height = image.size
+        doc.add_picture(BytesIO(bytes(signature)), width=Inches(width / 96))
 
-    # search for the image content control
-    pic_ccs = doc._element.xpath("//a:blip")
+        # Remove the paragraph containing the image
+        p = doc.paragraphs[-1]._element
+        p.getparent().remove(p)
 
-    # iterating over the list of all the picture content controls
-    for pic in pic_ccs:
-        pic.set(qn("r:embed"), rid)
-        try:
-            temp = doc._element.xpath("//w:sdtPr")[0]
-            temp.getparent().remove(temp)
-        except:
-            print("already gone")
+        # Finding the ID of the image
+        keys = list(doc.part.rels.keys())
+        rid = keys[-1]
 
-    return doc
+        # Find the content control with the specified name or skip if not found
+        for sdt in doc.element.xpath("//w:sdt"):
+            alias = sdt.xpath(".//w:alias[@w:val='%s']" % cc_name, namespaces=nsdecls)
+            if alias:
+                # Set the relationship ID for the image
+                sdt.xpath(".//a:blipFill/a:blip", namespaces=nsdecls)[0].set(qn("r:embed"), rid)
+                break
+
+    modified_docx_buffer = BytesIO()
+    doc.save(modified_docx_buffer)
+    modified_docx_content = modified_docx_buffer.getvalue()                 
+    return modified_docx_content
+
+
 
 def process_docx(fields:dict, docx_content:bytes) -> bytes:
     doc = Document(BytesIO(docx_content))
@@ -41,7 +61,6 @@ def process_docx(fields:dict, docx_content:bytes) -> bytes:
     for paragraph in doc.paragraphs:
         for cc in paragraph._element.xpath("w:sdt/w:sdtContent/w:r/w:t"):
             # Check each field for a match
-            i = 0
             for key,value in fields.items():
                 if cc.text == f"[{key}]":
                     cc.text = value
